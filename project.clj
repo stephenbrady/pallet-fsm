18284dcab14e11c3fd0ae9c2aaae06788e33415a
Stephen Brady <stephen.brady@gmail.com>
2014-11-20 23:37:03 -0800

    Update clojure required version to 1.6.0.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:pallet/pallet-fsm.git"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [pallet-thread "0.1.0"]]
